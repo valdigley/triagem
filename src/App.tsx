9118df6bb3ae9bdbd3a4a866dcfda10e93b9ac72
valdigley <valdigley2007@gmail.com>
2025-07-25 20:39:34 -0300

    Add missing Login component import

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
+import Login from './components/Login';
 import { DataProvider } from './contexts/DataContext';
 import Layout from './components/Layout';
 import Dashboard from './components/Dashboard';
